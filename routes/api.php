0b19a5cfdc3a122125cbdd71b2dc46105910fe44
Alexey Borovkov <lucky.aaz@gmail.com>
2019-04-01 14:48:27 +0300

    remove it

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('products', 'ProductController@search');
 Route::get('products/{id}', 'ProductController@show');
 
